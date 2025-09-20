8b516063c25f93b43af5f355e5fe5fb57acc548d
sanketg1234 <sanketghadge161205@gmail.com>
2025-09-20 16:45:34 +0530

    added user routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const usersController = require("../controllers/user.controller.js");
 
+//user routes
 router.post("/login", usersController.login);
 router.post("/createuser", usersController.createUser);
 
